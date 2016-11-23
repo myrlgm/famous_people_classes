0bcc041e32e0eed4b7db9ff2fdba3eb68808d6b4
Myrl Marmarelis <myrl@Myrls-MacBook-Pro.local>
2016-11-23 13:04:22 -0800

    working on db

--- server.js
+++ server.js
@@ -2,27 +2,64 @@ var express = require('express')
 var morgan  = require('morgan')
 var redis   = require('redis')
 
-var client = redis.createClient()
+var db = redis.createClient()
 var app = express()
 var template = require('jade').compileFile(__dirname + '/templates/main.jade')
 
 app.use(morgan('dev'))
 app.use(express.static(__dirname + '/static'))
 
-client.on('error', function(err) {
+db.on('error', function(err) {
  console.log('redis error: ' + err)
 })
 
+function get_instructor_name(id) {
+ return db.get(id + ' name')
+}
+
+function get_instructor_attributes(id) {
+ return db.smembers(id + ' attributes')
+}
+
+function get_video_url(id) {
+ return db.get(id + ' video_url')
+}
+
+function get_video_type(id) {
+ return db.get(id + ' video_type')
+}
+
+function get_other_courses(id) {
+ return db.smembers(id + ' other_courses')
+}
+
+function get_params(id) {
+ var params = {}
+ params.instructor_name = get_instructor_name(id)
+ params.title = params.instructor_name
+ params.attributes = get_instructor_attributes(id)
+ params.video_source = {
+  'src': get_video_url(id),
+ }
+ params.other_courses = []
+ var other_ids = get_other_courses(id)
+ for (i in other_ids) {
+  var id = other_ids[i]
+  params.other_courses.append({
+   'name': get_instructor_name(id),
+   'link': '/' + id
+  })
+ }
+ return params
+}
+
 app.get('/:id', function(req, res, next) {
   var id = req.params.id // should ideally do some filtering before going to DB
-  var params = {}
-  params.title = 'pie'
-  params.instructor_name = id
-  params.attributes = ['suh', 'hi', 'bar']
-  params.video_source = {'baz': 'foo'}
-  res.send(template(params))
+  res.send(template(get_params(id)))
 })
 
-app.listen(process.env.PORT || 3000, function () {
-  console.log('Listening on http://localhost:' + (process.env.PORT || 3000))
+var port = process.env.PORT || 3000
+
+app.listen(port, function () {
+  console.log('Listening on port: ' + port)
 })
